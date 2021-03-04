66996a7d5b228d9dcdd396537e2d769d4af58302
xirojidude <76069678+xirojidude@users.noreply.github.com>
2021-03-04 13:54:58 -0800

    reorganized scripts into folder. fixed door trigger script

---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,36 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class DoorTrigger : UdonSharpBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+       animator = GetComponentInChildren<Animator>();    
+    }
+
+   // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            animator.SetTrigger("isOpen");
+        } 
+        if (Input.GetKeyDown(KeyCode.T)) 
+        {
+            animator.ResetTrigger("isOpen");
+        }
+    }
+    private void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        animator.SetTrigger("isOpen");
+     }
+    private void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        animator.ResetTrigger("isOpen");
+     }
+}
