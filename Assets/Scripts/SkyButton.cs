9a1105f25317e1fdc9c9c680b053c47d7fc87be9
xirojidude <76069678+xirojidude@users.noreply.github.com>
2021-03-04 17:06:20 -0800

    Added buttons for changing the skybox

---
+++ Assets/Scripts/SkyButton.cs
@@ -0,0 +1,15 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class SkyButton : UdonSharpBehaviour
+{
+    public Material skybox;
+
+    public override void Interact()
+    {
+        RenderSettings.skybox = skybox;
+    }
+}
