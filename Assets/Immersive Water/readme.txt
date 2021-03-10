4350b2ae3d28ba374a1c66d7ae6faa9be8e4942e
xirojidude <76069678+xirojidude@users.noreply.github.com>
2021-03-10 14:36:56 -0800

    playing around with lighting

---
+++ Assets/Immersive Water/readme.txt
@@ -0,0 +1,15 @@
+-IMMERSIVE WATER-
+v3
+By Chai Latte
+
+Setup Instructions:
+1. Import VRCSDK, Standard Assets, Toybox, and Post Processing Stack to your project. 
+2. Set up VRC layers using the VRC Control Panel.
+3. Create a new layer in Slot 22 and call it PlayerTracking.
+4. Import the Immersive Water package. 
+5. Drag the scene (if you're a beginner) or prefab (if you're more comfortable experimenting) into your project. 
+
+Notes: 
+If you would like to use your own VRCWorld, make sure you have your main camera set as the reference camera.
+
+If you would like to use your own main camera, make sure you copy over the Post Process Layer component from the scene's main camera.
---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,42 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ShipController : UdonSharpBehaviour
+{
+    private Vector3 speed = new Vector3(0,0,0);
+    private Vector3 angularSpeed = new Vector3(0,0,0);
+    public Vector3 joystick = new Vector3(0,0,0);
+    private Vector3 maxAcceleration = new Vector3(0,0,0);
+    private Vector3 maxRotate = new Vector3(0,0,0);
+    public Vector3 drag = new Vector3(0,0,0);
+    public Vector3 angularDrag = new Vector3(0,0,0);
+
+    private VRCPlayerApi playerLocal;
+    private bool isActive;
+
+    void Start()
+    {
+        playerLocal = Networking.LocalPlayer;
+    }
+
+    private void onPickupUseDown() 
+    {
+        isActive = true;
+    }
+
+    private void onPickupUseUp()
+    {
+        isActive = false;
+    }
+
+    private void fixedUpdate()
+    {
+        if(isActive) 
+        {
+            playerLocal.SetVelocity(Vector3.ClampMagnitude(playerLocal.GetVelocity() + transform.forward, 50));
+        }
+    }
+}
