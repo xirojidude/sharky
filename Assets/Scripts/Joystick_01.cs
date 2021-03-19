44631bf898b1062a535619104384f0e9334cece2
xirojidude <76069678+xirojidude@users.noreply.github.com>
2021-03-18 17:09:02 -0700

    Added momentum and physics to flight

--- Assets/Scripts/Joystick_01.cs
+++ Assets/Scripts/Joystick_01.cs
@@ -9,7 +9,13 @@ public class Joystick_01 : UdonSharpBehaviour
 
     //[SerializeField] Transform Ship;
     public Transform Ship;
-    private float speed = 0.0f;
+    public float speed = 0.0f;
+    public float vspeed = 0.0f;
+    public int mode = 0;
+    public float maxSpeed = 50.0f;
+    public float acceleration = 0.005f;
+    public float deceleration = 0.005f;
+    private float thrust = 0.0f;
     private VRCPlayerApi player;
     private float smooth = 1.1f;
     private Vector3 joystickSpawnPosition;
@@ -18,6 +24,7 @@ public class Joystick_01 : UdonSharpBehaviour
     void Start()
     {
         player = Networking.LocalPlayer;
+//        joystickSpawnPosition = new Vector3(1.7899f,-0.5435001f,0.03769994f);
         joystickSpawnPosition = this.transform.localPosition;
         joystickSpawnRotation = this.transform.localRotation;
     }
@@ -69,22 +76,32 @@ public class Joystick_01 : UdonSharpBehaviour
 //        Ship.rotation = (Quaternion.Slerp(Ship.rotation, Quaternion.Euler(0.0f, Ship.rotation.y,Ship.rotation.z),  Time.deltaTime * smooth));
         //Ship.Rotate( Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal") );
         }
+
+        if (mode == 1)  
+        {
+            speed = Mathf.Min(maxSpeed,speed+acceleration+(acceleration*speed));
+        }
+            else 
+        {
+            speed = Mathf.Max(0.0f,speed-deceleration-(deceleration*speed));
+        }
+        vspeed = Mathf.Max(0.0f,vspeed-deceleration);
+        Ship.position += transform.up * Time.deltaTime * vspeed;
     }
 
     void OnPickupUseDown()
     {
-        speed = 40.0f;
+        mode = 1;
     }
 
     void OnPickupUseUp()
     {
-        speed = 0.0f;
+        mode = 0;
     }
 
     void OnPickup()
     {
-        Ship.position += transform.forward * Time.deltaTime * 40.0f;
-
+        vspeed = 2.0f;
         Ship.Rotate( Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal") );
 
 //        float terrainHeightWhereWeAre = Terrain.activeTerrain.SampleHeight( transform.position );
@@ -100,5 +117,9 @@ public class Joystick_01 : UdonSharpBehaviour
         // reset position of joystick to original
         this.transform.localPosition = joystickSpawnPosition;
         this.transform.localRotation = joystickSpawnRotation;
+        this.speed = 0.0f;
+        this.mode = 0;
+        this.vspeed = 0.0f;
+
     }
 }
