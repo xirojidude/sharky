3ca4a0c91cb6b9ac0c8d07cadf4015d1c578ca31
xirojidude <76069678+xirojidude@users.noreply.github.com>
2021-03-21 16:25:54 -0700

    Revert "Added momentum and physics to flight"
    This reverts commit 44631bf898b1062a535619104384f0e9334cece2.

--- Assets/Scripts/RespawnShipButton.cs
+++ Assets/Scripts/RespawnShipButton.cs
@@ -7,23 +7,17 @@ using VRC.Udon;
 public class RespawnShipButton : UdonSharpBehaviour
 {
     public GameObject Target;
-    public GameObject SpawnLocation;
     private Vector3 originalPostion;
     private Quaternion originalRotation;
 
     void Start()
     {
         originalPostion = Target.transform.position;    
-        //originalPostion = new Vector3(-7.2329f,2.19f,-1.083f);
-        originalRotation = Target.transform.rotation;
-        //7.2329,2.19,-1.083
     }
 
       void Interact()
     {
-//        Target.transform.position=originalPostion;
-//        Target.transform.rotation=originalRotation;
-        Target.transform.position=SpawnLocation.transform.position;
-        Target.transform.rotation=SpawnLocation.transform.rotation;
+        Target.transform.position=originalPostion;
+        Target.transform.rotation=originalRotation;
     }
 }
